ef86a38b95e68db54ebfb8eedd5760edc8180734
creperkiler2101 <50651572+creperkiler2101@users.noreply.github.com>
2019-10-13 23:51:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+//Init Github Class
+const github = new Github;
+//Init UI Class
+const ui = new UI;
+
+window.onload = function() {
+    //get the input text from the searchUser input field
+    const userText = "creperkiler2101";
+    if(userText !== '') {
+        //make an http call to GitHub API
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                //Show alert if user was not found
+                ui.showAlert('User Not Found', 'alert alert-danger');
+            } else {
+                //Show user profile and repos
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+            console.log(data);
+        })
+    }else {
+            ui.clearProfile();
+        }
+}
+
+
+
+
+
+
+
+/*loadEventListeners();
+
+function loadEventListeners() {
+    document.addEventListener('DOMContentLoaded', getObject);
+}
+
+function getObject() {
+    const gitHub = new Github;
+
+    gitHub.getUser('ju-voro')
+    .then(data=> {
+        console.log(data);
+    });
+}*/
